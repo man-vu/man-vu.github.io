440174ef771987bd05293bc2f520f034a60c4136
Man Vu <manvminh@gmail.com>
2025-07-06 00:37:02 -0400

    Update Vite configuration to set base path for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-}));
+});
